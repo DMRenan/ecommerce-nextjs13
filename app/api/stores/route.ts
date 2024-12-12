3df882bb2407127f92f9235e87e60eac2500fdcd
Renan Duarte <rnnmrcdrt@gmail.com>
2024-12-12 15:13:51 -0300

    added toast provider and adjusted routes

---
+++ app/api/stores/route.ts
@@ -0,0 +1,26 @@
+import prismadb from "@/lib/prismadb";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = getAuth(req);
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({ data: { name, userId } });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
