a65c40c40caa9c43da358d2cf5af71f7788f5777
Renan Duarte <rnnmrcdrt@gmail.com>
2025-02-04 21:43:03 -0300

    05:18:19

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,9 @@
 
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
